29a22110cb030a4c28f0581ca83e3a64f8b2b448
Your Name <faizanshah123123123@gmail.com>
2021-11-28 13:57:56 +0000

    renders images

--- website/api.py
+++ website/api.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, json, jsonify, request
+from flask import Blueprint, json, jsonify, request, send_file
 from .models import Post
 from werkzeug.utils import secure_filename
 from . import db
 import os
+import random
 
 api = Blueprint("api", __name__)
 
@@ -12,6 +13,27 @@ ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "gif"}
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def random_filename(filename):
+    min_ = int(1e7)
+    max_ = 99999999
+    rand = str(random.randint(min_, max_)) + "." + filename.rsplit(".", 1)[1].lower()
+
+    # Check if the filename is already in the database
+    from sqlalchemy.orm import sessionmaker
+
+    db_session_maker = sessionmaker(bind=db.engine)
+    db_session = db_session_maker()
+    while (
+        db_session.query(Post).filter(Post.file_name == rand).limit(1).first()
+        is not None
+    ):
+        rand = (
+            str(random.randint(min_, max_)) + "." + filename.rsplit(".", 1)[1].lower()
+        )
+
+    return rand
 
 
 @api.route("/images/", methods=["GET", "POST"])
@@ -29,7 +51,7 @@ def images():
 
         if file and allowed_file(file.filename):
             print("saving")
-            filename = secure_filename(file.filename)
+            filename = random_filename(secure_filename(file.filename))
             file.save(os.path.join(UPLOAD_FOLDER, filename))
 
         post = Post(title=request.form["title"], file_name=filename)
@@ -51,3 +73,8 @@ def image(image_id):
         db.session.delete(post)
         db.session.commit()
         return jsonify({"message": "Image Deleted"}), 200
+
+
+@api.route("/files/<file_name>", methods=["GET"])
+def file(file_name):
+    return send_file(os.path.join(UPLOAD_FOLDER, file_name))
